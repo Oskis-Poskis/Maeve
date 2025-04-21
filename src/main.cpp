0918ba11e5337431d5f125654e0a53737e9bedb4
OskisPoskis <kassanderoskar@gmail.com>
2025-04-21 12:52:27 +0200

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,16 @@
 #include "engine/render_engine.h"
 #include "engine/asset_manager.h"
 #include "engine/scene_manager.h"
-#include "common/qk.h"
 
 int main()
 {
     Engine::Initialize();
  
-    AssetManager::IO::LoadObj("res/objs/armadillo.obj",      "loadedmodel0");
-    AssetManager::IO::LoadObj("res/objs/cube.obj",           "loadedmodel1");
-    AssetManager::IO::LoadObj("res/objs/teapot.obj",         "loadedmodel2");
-    AssetManager::IO::LoadObj("res/objs/sphere.obj",         "loadedmodel3");
-    AssetManager::IO::LoadObj("res/objs/teapot.obj",         "loadedmodel4");
-    AssetManager::IO::LoadObj("res/objs/suzanne.obj",        "loadedmodel5");
-    AssetManager::IO::LoadObj("res/objs/suzanne_smooth.obj", "loadedmodel6");
+    AM::IO::LoadObj("res/objs/suzanne_smooth.obj", "loaded1");
+    SM::Object obj("Loaded Model", "loaded1");
+    SM::AddNode(&obj);
 
-    SceneManager::Light light1("light1", SceneManager::LightType::Point, glm::vec3(0, 3, 0),  10.0f, glm::vec3(1, 1, 1), 10.0f);
-    SceneManager::Light light2("light2", SceneManager::LightType::Point, glm::vec3(-7, 3, 0), 10.0f, glm::vec3(0, 1, 1),  2.5f);
-    SceneManager::Light light3("light3", SceneManager::LightType::Point, glm::vec3(7, 3, 0),  10.0f, glm::vec3(1, 0, 0),  5.0f);
-
-    SceneManager::AddLight(light1);
-    SceneManager::AddLight(light2);
-    SceneManager::AddLight(light3);
-
-    int num = 5;
+    /* int num = 5;
     int spacing = 4;
     for (int i = 0; i < num * num * num; i++)
     {
@@ -31,11 +18,18 @@ int main()
         int ydir = i /(num * num);
         int zdir = (i / num) % num;
 
-        std::string mesh = "loadedmodel6"; // std::to_string(qk::RandomInt(0, 6))
-        SceneManager::Object myobj("monkey_" + std::to_string(i), mesh);
-        myobj.SetPosition(glm::vec3(xdir * spacing - ((num - 1) * spacing) / 2.0f, ydir * spacing, zdir * spacing - ((num - 1) * spacing) / 2.0f));
-        SceneManager::AddObject(myobj);
-    }
+        std::string mesh = "suzanne";
+        SM::Object* myobj = new SM::Object("monkey_" + std::to_string(i), mesh);
+        myobj->SetPosition(glm::vec3(xdir * spacing - ((num - 1) * spacing) / 2.0f, ydir * spacing, zdir * spacing - ((num - 1) * spacing) / 2.0f));
+        SM::AddNode(myobj);
+    } */
+
+    SM::Light light1("Point Light1", SM::LightType::Point, glm::vec3( 0, 0, 4.0f), 10.0f, glm::vec3(1, 1, 1), 5.0f);
+    SM::Light light2("Point Light2", SM::LightType::Point, glm::vec3(-7, 0, 2.5f), 10.0f, glm::vec3(0, 1, 1), 5.0f);
+    SM::Light light3("Point Light3", SM::LightType::Point, glm::vec3( 7, 0, 2.5f), 10.0f, glm::vec3(1, 0, 0), 5.0f);
+    SM::AddNode(&light1);
+    SM::AddNode(&light2);
+    SM::AddNode(&light3);
  
     Engine::Run();
 }
